648f069777d7bb7ea296fdebccdacf96014cbfac
RiccardoLoPresti <riccardolopresti35@gmail.com>
2022-10-17 16:25:55 +0200

    [add] milestone-3

---
+++ milestone-3/js/script.js
@@ -0,0 +1,37 @@
+
+const imgList = [
+  "01.jpg",
+  "02.jpg",
+  "03.jpg",
+  "04.jpg",
+  "05.jpg"
+];
+
+let imgTag = '';
+
+const imgWrapper = document.querySelector('.img-wrapper');
+
+for(let i = 0; i < imgList.length; i++){
+  imgTag += `
+    <img class="items" src="img/${imgList[i]}" alt="${imgList[i]}">
+  `;
+}
+
+imgWrapper.innerHTML += imgTag;
+
+let elCounter = 0;
+const element = document.getElementsByClassName('items');
+
+element[elCounter].classList.add('active');
+
+
+
+
+
+
+
+
+
+
+
+
