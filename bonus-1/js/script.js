a6025271c35dc72e51f65c2615214d25b6eea0c0
RiccardoLoPresti <riccardolopresti35@gmail.com>
2022-10-17 21:56:52 +0200

    [dd] bonus-1

--- bonus-1/js/script.js
+++ bonus-1/js/script.js
@@ -35,35 +35,36 @@ elementCarousel[elCounter].classList.add('active');
 const prev = document.querySelector('.up');
 const next = document.querySelector('.down');
 
-if(elCounter === 0){
-  prev.classList.add('hide');
-}
-
 next.addEventListener('click', function(){
-  element[elCounter].classList.remove('active');
-  elementCarousel[elCounter].classList.remove('active');
-  elCounter++;
-  element[elCounter].classList.add('active');
-  elementCarousel[elCounter].classList.add('active');
-
-  prev.classList.remove('hide');
-  
-  if(elCounter === (imgList.length - 1)){
-    next.classList.add('hide');
+  if(elCounter >= imgList.length - 1){
+    elCounter = 0;
+    element[elCounter].classList.add('active');
+    element[imgList.length - 1].classList.remove('active');
+    elementCarousel[elCounter].classList.add('active');
+    elementCarousel[imgList.length - 1].classList.remove('active');
+  }else{
+    element[elCounter].classList.remove('active');
+    elementCarousel[elCounter].classList.remove('active');
+    elCounter++;
+    element[elCounter].classList.add('active');
+    elementCarousel[elCounter].classList.add('active');
   }
 });
 
 prev.addEventListener('click', function(){
-  element[elCounter].classList.remove('active');
-  elementCarousel[elCounter].classList.remove('active');
-  elCounter--;
-  element[elCounter].classList.add('active');
-  elementCarousel[elCounter].classList.add('active');
-
-  next.classList.remove('hide');
-
-  if(elCounter === 0){
-    prev.classList.add('hide');
+  if(elCounter <= 0){
+    console.log('dadada', elCounter);
+    elCounter = imgList.length - 1;
+    element[0].classList.remove('active');
+    element[imgList.length - 1].classList.add('active');
+    elementCarousel[0].classList.remove('active');
+    elementCarousel[imgList.length - 1].classList.add('active');
+  }else{
+    element[elCounter].classList.remove('active');
+    elementCarousel[elCounter].classList.remove('active');
+    elCounter--;
+    element[elCounter].classList.add('active');
+    elementCarousel[elCounter].classList.add('active');
   }
 });
 
